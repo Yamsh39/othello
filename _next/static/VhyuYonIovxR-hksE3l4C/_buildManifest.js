1075a47e1ad4274a3a15a16d1169f9e903d07677
Yamsh39 <Yamsh39@users.noreply.github.com>
2023-06-13 14:47:55 +0000

    deploy: 531fdf51fce2583a9a2adbeec0b8848612d981b1

--- _next/static/VhyuYonIovxR-hksE3l4C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/40bbea218d8eeea3.css","static/chunks/pages/index-af97a6c8fa5b7204.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
